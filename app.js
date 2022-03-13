4288bf63761b5d5eb8f5016b5bef0694fc13802a
Fer619 <fbriceno@ucol.mx>
2022-03-12 23:19:23 -0600

    Implementación de object y llamadas console

---
+++ app.js
@@ -0,0 +1,10 @@
+let obj= {
+    nombre:'Fernando',
+    edad: 28,
+    apellido: 'Briceño'
+}
+
+console.log(obj.nombre);
+console.log(obj['nombre']);
+
+// los console.log regresan el valor de nombre en ambos casos
